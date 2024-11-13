30f21881dbcf8049d5b3264ca769e671522a064f
Toan <thaitoan3039015@gmail.com>
2024-11-13 15:11:02 +0700

    test husky

--- src/Presentation/server/app.module.ts
+++ src/Presentation/server/app.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 
 
+import { DatabaseModule } from '@Infrastructure/database/database.module';
+
 import { AuthModule } from '@Presentation/server/module/auth';
 import { CommonModule } from '@Presentation/server/module/common';
-import { DatabaseModule } from '@Infrastructure/database/database.module';
 import { UserModule } from '@Presentation/server/module/user';
 
 
 
 
 
+
+
+
+
 @Module({
-          imports: [DatabaseModule, CommonModule, AuthModule, UserModule],
+  imports: [DatabaseModule, CommonModule, AuthModule, UserModule],
 })
-export class AppModule {}
+export class AppModule { }
